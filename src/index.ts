538e28c32a46195d8123acee936a099eb26ab9d5
Selman Ciftci <selman.ciftci.coding@gmail.com>
2024-02-12 10:09:02 +0100

    Add @types/cors package and update index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,29 @@
 import express from 'express'
 import cors from 'cors'
+import { MemberType } from '../types'
 
 const app = express()
 const port = 3311
 
 app.use(cors())
 
+const members: MemberType[] = [
+    {
+        id: 1,
+        name: 'Robert Hammerschmidt',
+    },
+    {
+        id: 2,
+        name: 'Angela Wasserturm',
+    },
+]
+
+app.get('/members', (req, res) => {
+    res.json(members)
+})
+
 app.listen(port, () => {
     console.log(`listening at http://localhost:${port}`)
 })
 
-console.log(`typescript backend`)
+console.log(`typescript backend is running`)
