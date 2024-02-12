03f3663d56e6a28b4b8422ab215d6b1a511620c3
Selman Ciftci <selman.ciftci.coding@gmail.com>
2024-02-12 10:04:46 +0100

    deletes js file and uses index ts instead

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,13 @@
+import express from 'express'
+import cors from 'cors'
+
+const app = express()
+const port = 3311
+
+app.use(cors())
+
+app.listen(port, () => {
+    console.log(`listening at http://localhost:${port}`)
+})
+
 console.log(`typescript backend`)
